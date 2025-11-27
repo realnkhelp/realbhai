28737e7db668f4298e9884ea23b0219ab8d9935b
realnkhelp <rajababu24553@gmail.com>
2025-11-27 15:43:34 +0530

    update next.config.mjs

--- X-Engagement-Bot-main/next.config.mjs
+++ X-Engagement-Bot-main/next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
